0ac20dae3f650b516d0c88e61e4c72c188a80fc4
cahyo <cahyoedisaputra@students.amikom.ac.id>
2026-04-09 11:23:39 +0700

    selesai

--- calculator.cs
+++ calculator.cs
@@ -2,5 +2,24 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
+		int a = 10;
+		int b = 6;
+		
+		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penamabahan(a, b));
+		Console.WriteLine("Hasil Penambahan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+		
+		Console.WriteLine("h";
+		Console.ReadKey();
+		
+	}
+	
+	static int Penamabahan(int a, int b)
+	{
+		return a + b;
+	}
+	
+	static int Pengurangan(int a, int b)
+	{
+		return a - b;
 	}
 }
